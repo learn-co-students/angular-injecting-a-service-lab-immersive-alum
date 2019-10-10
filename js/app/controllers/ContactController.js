965993aaa82bb46a61758e4a9c511024b26cb85b
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-10-10 13:25:13 +0000

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Rebecca';
+	
+	$timeout(function() {
+	  $scope.name = 'Charlotte';
+	}, 5000);
 }
 
 angular
